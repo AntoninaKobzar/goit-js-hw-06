3bdbd38cbb8610a0761d022cd1006eec849a4125
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-04 20:56:02 +0200

    task6

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,9 +1,9 @@
 
-    const listOfCategories = document.querySelector("#categories");
-    const listOfClassItem = listOfCategories.querySelectorAll(".item");
-    console.log(`Number of categories: ${listOfClassItem.length}`);
-    listOfClassItem.forEach((item) => {
-        console.log(`Category: ${item.firstElementChild.textContent}`);
-        console.log(`Elements: ${item.lastElementChild.children.length}`)
-    });
+        const listOfCategories = document.querySelector("#categories");
+        const listOfClassItem = listOfCategories.querySelectorAll(".item");
+        console.log(`Number of categories: ${listOfClassItem.length}`);
+        listOfClassItem.forEach((item) => {
+            console.log(`Category: ${item.firstElementChild.textContent}`);
+            console.log(`Elements: ${item.lastElementChild.children.length}`)
+        });
 
