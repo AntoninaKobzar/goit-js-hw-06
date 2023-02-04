85d42d96e031bcc691404e8b7657cb68ab617184
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-04 19:30:42 +0200

    task5

---
+++ src/src/js/task-05.js
@@ -0,0 +1,8 @@
+    let inputEl=document.querySelector("#name-input");
+    let outputEl=document.querySelector("#name-output");
+    inputEl.addEventListener("input",(event=>{
+        outputEl.textContent=event.currentTarget.value;
+    }));
+
+
+
