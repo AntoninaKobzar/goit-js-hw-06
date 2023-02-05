712c0f3a49fc4e88c590440c55adffa2a954189c
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-05 21:55:38 +0200

    task 8 changed

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,21 @@
         const form=document.querySelector(".login-form");
         form.addEventListener("submit",handleSubmit);
         function handleSubmit(event){
-            event.preventDefault();
-            const email = event.currentTarget.elements.email.value;
-            const password = event.currentTarget.elements.password.value;
-            const elements={email,password,};
-            if(email.value === "" || password.value === ""){
-            return alert("Please, fill in all the fields!");
-            }
-            console.log(elements)
-            event.currentTarget.reset();
+        event.preventDefault();
+        const email = event.currentTarget.elements.email.value;
+        const password = event.currentTarget.elements.password.value;
+        const elements = {
+            email,
+            password,
+        };
+        email === "" || password === ""?
+        alert("Please, fill in all the fields!"):
+        console.log(elements)
+        event.currentTarget.reset();
         }
+        form.addEventListener("submit", handleSubmit);
+
+
 
 
 
